b16b2e2089d634a83bf0a79a459ce5d19c96f905
Navarro <altnavarro@gmail.com>
2017-06-16 12:43:44 +0300

    Добавлен UserControl

--- April.UI/FormMain.Designer.cs
+++ April.UI/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace April.UI
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 541);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
