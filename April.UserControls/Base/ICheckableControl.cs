561348728b20c9b0ccec0e504d4cf0a26a01f03d
Navarro <altnavarro@gmail.com>
2017-06-18 13:06:21 +0300

    Добавлен интерфейс ICheckableControl, добавлен компонент-наследник CustomTextBox

---
+++ April.UserControls/Base/ICheckableControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace April.UserControls.Base
+{
+    public interface ICheckableControl
+    {
+        /// <summary>
+        /// Проверять или нет компонент на то, что данные не пустые.
+        /// </summary>
+        bool EmptyDataCheck { get; set; }
+
+        /// <summary>
+        /// Текст, который будет выводиться пользователю, если данные не заполнены.
+        /// </summary>
+        string EmptyDataText { get; set; }
+
+        /// <summary>
+        /// Проверять на наличие данных в компоненте
+        /// </summary>
+        /// <returns></returns>
+        bool Check();
+    }
+}
---
+++ April.UserControls/CustomControls/CustomTextBox.cs
@@ -0,0 +1,26 @@
+﻿using April.UserControls.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace April.UserControls.CustomControls
+{
+    public class CustomTextBox
+        : TextBox, ICheckableControl    {
+        public bool EmptyDataCheck { get; set; }
+
+        public string EmptyDataText { get; set; }
+
+        public bool Check()
+        {
+            if (!EmptyDataCheck) return true;
+            if (this.Text != null && this.Text != string.Empty) return true;
+
+            MessageBox.Show(EmptyDataText);
+            return false;
+        }
+    }
+}
